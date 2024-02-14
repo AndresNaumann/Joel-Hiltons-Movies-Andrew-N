73e2b7056422533f0ccb41342adc85daa3fcf521
Drew Naumann <naumannadn@gmail.com>
2024-02-14 10:25:05 -0700

    Add project files.

---
+++ Joel-Hiltons-Movies/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Joel_Hiltons_Movies.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Joel_Hiltons_Movies.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Joel-Hiltons-Movies/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Joel_Hiltons_Movies.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
